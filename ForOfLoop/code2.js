9d74418a0e8d340efec38e3421b0bf5a51a592c5
Your Name <you@example.com>
2026-07-07 06:47:24 +0530

    updated the comments for better explaination of frequency of string in for of loop using bracket and dot notation

--- ForOfLoop/code2.js
+++ ForOfLoop/code2.js
@@ -19,6 +19,15 @@ charCount.char=1
  // Print results
   for (let key in charCount) {
     console.log(`${key} ${charCount[key]}`);//output : char 10
+    // charCount.char always refers to a property literally named "char". 
+    // aur char property ka naam "char" hai, isliye har loop iteration me bas usi ek property
+    //  ko increment kiya ja raha hai. so else pe loop bss 1st iteration me hi jaa raha hai aur charCount.char ki value 1 ho ja rahi hai. kyki 1st 
+    // iteration ke time charCount object me char property nahi hai, isliye else block execute ho raha hai aur charCount.char ki value 1 ho rahi hai.
+    // and later iterations me charCount.char property already exist kar rahi hai, isliye if block execute ho raha hai aur charCount.char ki value 1 se increment ho kar 2 ho ja rahi hai.
+
+// Every loop iteration just increments that one property.
+
+// Since "javascript" has 10 characters, the final result is "char": 10.
   }
   return charCount
 }
@@ -43,7 +52,16 @@ charCount[char]=1
 }
  // Print results
   for (let key in charCount) {
-    console.log(`${key} ${charCount[key]}`);//output : char 10
+    console.log(`${key} ${charCount[key]}`);
+//output : j 1
+// a 2
+// v 1
+// s 1
+// c 1
+// r 1
+// i 1
+// p 1
+// t 1
   }
   return charCount
 
